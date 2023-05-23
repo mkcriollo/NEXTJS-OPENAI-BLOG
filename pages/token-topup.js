4354db9af7f75d5d31fcb97c0fd137f3f6ec078e
Mike Criollo <mkcriollo@gmail.com>
2023-05-23 16:44:37 -0400

    style call to action and token links on header of sidebar

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,8 +1,13 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../components/AppLayout";
 
 export default function TokenTopup() {
   return <div>Hello this is TokenTopup Page</div>;
 }
+
+TokenTopup.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
 
 export const getServerSideProps = withPageAuthRequired(() => {
   return {
